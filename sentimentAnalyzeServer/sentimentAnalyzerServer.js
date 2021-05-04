fdc096bd845b293f07016657ffa7435e74fff5af
coursera-al <slh58130@eoopy.com>
2021-05-04 00:03:02 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,78 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors_app = require('cors');
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+dotenv.config()
+
 const app = new express();
 
 app.use(express.static('client'))
-
-const cors_app = require('cors');
 app.use(cors_app());
 
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
 
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+app.get("/url/emotion", async (req,res) => {
+    const response = await getNLUInstance().analyze({
+       url: req.query.url,
+       features: {
+           emotion: {
+               document: true
+           }
+       }
+    });
+    return res.json(response.result.emotion.document.emotion);
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/sentiment", async (req,res) => {
+    const response = await getNLUInstance().analyze({
+       url: req.query.url,
+       features: {
+           sentiment: {
+               document: true
+           }
+       }
+    });
+    return res.send(response.result.sentiment.document.label);
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/text/emotion", async (req,res) => {
+    const response = await getNLUInstance().analyze({
+       text: req.query.text,
+       features: {
+           emotion: {
+               document: true
+           }
+       }
+    });
+    return res.json(response.result.emotion.document.emotion);
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/sentiment", async (req,res) => {
+    const response = await getNLUInstance().analyze({
+       text: req.query.text,
+       features: {
+           sentiment: {
+               document: true
+           }
+       }
+    });
+    return res.send(response.result.sentiment.document.label);
 });
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+function getNLUInstance () {
+    return new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: process.env.API_KEY
+        }),
+        serviceUrl: process.env.API_URL
+    });
+}
